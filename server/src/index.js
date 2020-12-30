f3af0001eee5cf994e21ec7f13c48a61a1da3287
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-12-30 14:03:08 -0700

    feat: :sparkles: Add dot env module for enviroment variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { PORT } from './config';
 
 const app = express();
 
-app.listen(3000, () => console.log('Server on'));
+app.get('/', (req, res) => res.send('working'));
+
+app.listen(PORT, () => console.log('Server on'));
