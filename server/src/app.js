4d853468971c0ae4a6527e89b2682c0130668521
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-30 17:58:27 -0600

    feat(mongoose): :sparkles:  database connection

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import database from './config/database';
 
 // load env variables
 dotenv.config();
 
 // create an express application
 const app = express();
+
+// database connection
+database();
 
 // middlewares
 app.use(express.json());
