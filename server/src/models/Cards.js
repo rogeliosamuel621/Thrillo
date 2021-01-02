1821e043056623fc785a181931c9c0730bc7ee15
Senix Code <codesenix@gmail.com>
2021-01-02 13:12:11 -0500

    feat(models):  :sparkles: New property model card

--- server/src/models/Cards.js
+++ server/src/models/Cards.js
@@ -2,6 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const cardSchema = new Schema({
   boardId: { type: String, required: true },
+  todoId: { type: Schema.Types.ObjectId, ref: 'todo', required: true },
   title: { type: String, required: true },
   description: { type: String, required: true }
 });
