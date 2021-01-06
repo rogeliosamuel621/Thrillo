35873795a5122d323773ca6a543ad44019786ba0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-05 18:29:40 -0700

    feat(styles): :sparkles: add "add icon"

--- client/src/components/pages/Board.jsx
+++ client/src/components/pages/Board.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import "../../styles/pages/board.css"
+import React from 'react';
+import '../../styles/pages/board.css';
 import { BoardCard } from '../molecules/';
-const Board = () => {
-    return (
-        <>
-            <div className="container">
-                <div className="header flex">
-                    <p>All board</p>
-                    <button className="button">
-                        add
-            </button>
-                </div>
-                <div className="container-board">
-                    <BoardCard />
-                    <BoardCard />
-                    <BoardCard />
-                    <BoardCard />
-                </div>
-            </div>
-        </>
-    )
-}
+import AddIcon from '../../assets/img/add.svg';
 
-export default Board
+const Board = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="header flex">
+          <p>All board</p>
+          <button className="button">
+            <img src={AddIcon} alt="add icon" width="15px" height="15px" />
+            add
+          </button>
+        </div>
+        <div className="container-board">
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+          <BoardCard />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Board;
