ead394ac0da25c4aadbd517b8a247bb1b46043aa
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-10 18:14:35 -0700

    feat: :sparkles: add fake api

---
+++ client/src/utils/fakeApi.js
@@ -0,0 +1,42 @@
+export const fakeBoards = [
+  {
+    img:
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608400402/My%20Website/AuthApp_ivdwwq.png',
+    title: 'Dev challenges',
+    teamMates: [
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+    ],
+  },
+  {
+    img:
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608400402/My%20Website/AuthApp_ivdwwq.png',
+    title: 'Dev challenges',
+    teamMates: [
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+    ],
+  },
+  {
+    img:
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608400402/My%20Website/AuthApp_ivdwwq.png',
+    title: 'Dev challenges',
+    teamMates: [
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+    ],
+  },
+  {
+    img:
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608400402/My%20Website/AuthApp_ivdwwq.png',
+    title: 'Dev challenges',
+    teamMates: [
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1608145205/My%20Website/myLogo_hc28py.svg',
+    ],
+  },
+];
