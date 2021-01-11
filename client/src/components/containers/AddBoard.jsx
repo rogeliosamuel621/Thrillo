d18998e1f5203c7642207f45fb458ae17cb10f5a
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-10 17:57:35 -0700

    feat(modal): :sparkles: user can see a image preview of the image

--- client/src/components/containers/AddBoard.jsx
+++ client/src/components/containers/AddBoard.jsx
@@ -7,14 +7,17 @@ const AddBoard = () => {
   const [title, setTitle] = useState('');
 
   function handleFileSelected(e) {
-    const files = Array.from(e.target.files);
-    console.log(files);
-    setFileName(files[0].name);
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => {
+      setFileName(reader.result);
+    };
   }
   return (
     <div className="container-addBoard">
       <div>
-        <img href="#" className="img-board" />
+        <img src={fileName} className="img-board" />
       </div>
       <div>
         <Input
