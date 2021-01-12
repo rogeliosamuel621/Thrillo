e91c2129a7c96cb67bdb648d38f83e30bf29fd6b
Angel Martinez <angel.martineztrc@gmail.com>
2021-01-12 17:24:00 -0600

    feat(middlewares): :sparkles:  adding a handleBody middleware for check values in body

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,12 +1,24 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 
+// middlewares
+import handleBody from '../middlewares/handleBody';
 // controllers
 import { UserController } from '../controllers';
 
 const router = Router();
 
 function UserRoutes() {
-  router.post('/register', UserController.create);
+  router.post(
+    '/register',
+    [
+      body('username', 'Username is required').notEmpty(),
+      body('email', 'Insert a valid email').isEmail(),
+      body('password', 'Password must have 6 or more characters').isLength({ min: 6 }),
+      handleBody
+    ],
+    UserController.create
+  );
 
   return router;
 }
