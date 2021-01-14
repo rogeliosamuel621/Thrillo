571519db81edeb64f8100fb8fef93fa086653458
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-14 14:42:41 -0700

    feat(Containers): :sparkles: Add login form

--- client/src/components/containers/index.js
+++ client/src/components/containers/index.js
@@ -1,5 +1,6 @@
 import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 import AddBoard from './AddBoard';
 import ButtonAddBoard from './ButtonAddBoard';
 
-export { RegisterForm, AddBoard, ButtonAddBoard };
+export { RegisterForm, AddBoard, ButtonAddBoard, LoginForm };
