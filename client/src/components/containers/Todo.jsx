d9edeb93acae690cbf784d647c3fff1e43c4f8f7
senixcode <codesenix@gmail.com>
2021-01-16 16:09:09 -0500

    feat(component): :sparkles: Added container Todo

---
+++ client/src/components/containers/Todo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SectionColumns = () => {
+  const CardTask = () => <div className="card-task">task test</div>;
+  const Column = () => (
+    <div className="column">
+      <CardTask />
+    </div>
+  );
+  return (
+    <div className="container-columns">
+      <Column />
+      <Column />
+      <Column />
+    </div>
+  );
+};
+const SectionAddColumns = () => (
+  <div className="container-add-columns">
+    <button>Add another list</button>
+  </div>
+);
+
+const Todo = () => {
+  return (
+    <div className="container-todo">
+      <SectionColumns />
+      <SectionAddColumns />
+    </div>
+  );
+};
+
+export default Todo;
