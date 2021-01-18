57a591f51264d47fd8054e11e04179781c369f17
Angel Martinez <angel.martineztrc@gmail.com>
2021-01-18 14:53:00 -0600

    feat(auth.controller): :sparkles:  authenticate the user with email and password

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,1 +1,34 @@
-export const authenticate = () => {};
+import argon2 from 'argon2';
+
+// models
+import User from '../models/User';
+
+// utils
+import ErrorHandler from '../utils/errorHandler';
+
+export const authenticate = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(new ErrorHandler(400, 'Email or Password incorrect'));
+    }
+
+    // check if the password match
+    const isGoodPassword = await argon2.verify(user.password, password);
+    if (!isGoodPassword) {
+      return next(new ErrorHandler(400, 'Email or Password incorrect'));
+    }
+
+    // generate the token
+
+    // return the response
+    return res.status(200).json({
+      message: 'Authenticated successfully'
+    });
+  } catch (error) {
+    return next(new ErrorHandler(500, 'Internal server error'));
+  }
+};
