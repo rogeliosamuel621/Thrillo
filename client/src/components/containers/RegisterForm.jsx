6c1bd57950879644a4ffe67de000c9c1eac7d68a
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-19 17:04:07 -0700

    feat(Register): :sparkles: register is working with api

--- client/src/components/containers/RegisterForm.jsx
+++ client/src/components/containers/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/containers/registerForm.css';
+import axios from 'axios';
 
 import { Input } from '../atoms/';
 
@@ -8,13 +9,24 @@ const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function register(e) {
+    e.preventDefault();
+
+    const data = { username, email, password };
+
+    axios
+      .post('http://localhost:3000/api/users/register', data)
+      .then((res) => console.log(res));
+  }
+
   return (
     <div className="RegisterForm-container">
       <div className="RegisterForm">
         <div className="RegisterForm-header">
           <h1>Create an account</h1>
         </div>
-        <form className="RegisterForm-form">
+        <form className="RegisterForm-form" onSubmit={register}>
           <div className="RegisterForm-form-inputContainer">
             <Input
               placeHolder="Username"
